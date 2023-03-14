f4f746883b476967616862979e87e5532413a219
vasshil <qwertyvasya02@gmail.com>
2023-03-15 00:03:27 +0300

    done

--- src/main/kotlin/com/example/laba5/Application.kt
+++ src/main/kotlin/com/example/laba5/Application.kt
@@ -4,11 +4,14 @@ import javafx.application.Application
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
 import javafx.stage.Stage
+import java.net.HttpURLConnection
+import java.net.URL
 
 class HelloApplication : Application() {
     override fun start(stage: Stage) {
-        val fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("hello-view.fxml"))
-        val scene = Scene(fxmlLoader.load(), 320.0, 240.0)
+
+        val fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("view.fxml"))
+        val scene = Scene(fxmlLoader.load())
         stage.title = "Лаба 5"
         stage.scene = scene
         stage.show()
@@ -17,4 +20,6 @@ class HelloApplication : Application() {
 
 fun main() {
     Application.launch(HelloApplication::class.java)
+
+
 }
--- src/main/kotlin/com/example/laba5/Controller.kt
+++ src/main/kotlin/com/example/laba5/Controller.kt
@@ -1,14 +1,83 @@
 package com.example.laba5
 
+import javafx.collections.FXCollections
+import javafx.collections.ObservableList
 import javafx.fxml.FXML
+import javafx.scene.control.ChoiceBox
 import javafx.scene.control.Label
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.URL
 
-class HelloController {
-    @FXML
-    private lateinit var welcomeText: Label
+class Controller {
+
+    private val usdUrl = "https://quote.ru/ticker/59111"
+    private val eurUrl = "https://quote.ru/ticker/59090"
+    private val cnuUrl = "https://quote.ru/ticker/59066"
+
+    private val currenciesList = listOf("Доллар", "Евро", "Юань")
 
     @FXML
-    private fun onHelloButtonClick() {
-        welcomeText.text = "Welcome to JavaFX Application!"
+    private lateinit var currencySelector: ChoiceBox<String>
+
+    @FXML
+    private lateinit var priceLabel: Label
+
+    @FXML
+    fun initialize() {
+//        getPrice(getHTML(usdUrl))
+//        getPrice(getHTML(eurUrl))
+//        getPrice(getHTML(cnuUrl))
+
+        currencySelector.items = FXCollections.observableArrayList(currenciesList)
+        currencySelector.setOnAction {
+            when(currencySelector.value) {
+                currenciesList[0] -> {
+                    setPrice(usdUrl)
+                }
+                currenciesList[1] -> {
+                    setPrice(eurUrl)
+                }
+                currenciesList[2] -> {
+                    setPrice(cnuUrl)
+                }
+            }
+        }
+
+        currencySelector.value = currenciesList[0]
     }
+
+    private fun getHTML(url: String): String {
+        val urlObj = URL(url)
+        val urlConnection = urlObj.openConnection() as HttpURLConnection
+
+        var text = ""
+
+        try {
+            text = urlConnection.inputStream.bufferedReader().readText()
+
+        } catch (e: IOException) {
+            priceLabel.text = "Ошибка"
+
+        } finally {
+            urlConnection.disconnect()
+
+        }
+
+        return text
+    }
+
+    private fun setPrice(url: String) {
+
+        val html = getHTML(url)
+
+        var price = html.substring(html.indexOf("₽<!-- --> <!-- -->") + 18, html.length)
+        price = price.substring(0, price.indexOf("<"))
+
+        println(price)
+
+        priceLabel.text = price
+    }
+
+
 }
